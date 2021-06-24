0ddf3a0133ff7d82eb0401307d73954fbd189b2b
Chris <chirssel@gmail.com>
2021-06-24 19:40:30 +0200

    Stop identifier regex from conflicting with number regex

--- lisp/token.go
+++ lisp/token.go
@@ -37,7 +37,7 @@ var Patterns = map[TokenType]*regexp.Regexp{
 	OpenToken:       regexp.MustCompile("^[({]"),
 	CloseToken:      regexp.MustCompile("^[)}]"),
 	NumberToken:     regexp.MustCompile("^[+-]?\\d+"),
-	IdentifierToken: regexp.MustCompile("^[a-zA-Z0-9_+\\-*/\\\\=<>!&]+"),
+	IdentifierToken: regexp.MustCompile("^[a-zA-Z_+\\-*/\\\\=<>!&][a-zA-Z0-9_+\\-*/\\\\=<>!&]*"),
 }
 
 type Token struct {
