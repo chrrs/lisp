e9738f6c820ef29961022d1c48f19c7772b81664
Chris <chirssel@gmail.com>
2021-06-25 22:00:12 +0200

    Load stdlib in the REPL at startup

--- lisp.go
+++ lisp.go
@@ -15,6 +15,15 @@ func main() {
 	env := lisp.NewEnvironment(nil)
 	env.AddBuiltins()
 
+	std, err := os.ReadFile("lib/std.clsp")
+	if err == nil {
+		_, err := Evaluate(&env, string(std), true)
+		if err != nil {
+			fmt.Printf("error while evaluating std.clsp: %v", err)
+			return
+		}
+	}
+
 	for {
 		fmt.Print("> ")
 
@@ -23,18 +32,42 @@ func main() {
 		}
 
 		input := scanner.Text()
-		tokens, err := lisp.Tokenize(input)
+		out, err := Evaluate(&env, input, false)
 		if err != nil {
-			fmt.Println("tokenization error:", err)
-			continue
+			fmt.Println(err)
+		} else {
+			fmt.Println(out)
+		}
+	}
+}
+
+func Evaluate(env *lisp.Environment, input string, multi bool) (lisp.Node, error) {
+	tokens, err := lisp.Tokenize(input)
+	if err != nil {
+		return nil, fmt.Errorf("tokenization error: %v", err)
+	}
+
+	if multi {
+		expression, err := lisp.ParseExpression(tokens, lisp.QExpression)
+		if err != nil {
+			return nil, fmt.Errorf("parsing error: %v", err)
 		}
 
+		for _, node := range expression.Nodes {
+			out := node.Evaluate(env)
+			err, ok := out.(lisp.ErrorNode)
+			if ok {
+				return nil, err.Error
+			}
+		}
+
+		return lisp.ExpressionNode{Type: lisp.SExpression, Nodes: make([]lisp.Node, 0)}, nil
+	} else {
 		expression, err := lisp.ParseExpression(tokens, lisp.SExpression)
 		if err != nil {
-			fmt.Println("parsing error:", err)
-			continue
+			return nil, fmt.Errorf("parsing error: %v", err)
 		}
 
-		fmt.Println(expression.Evaluate(&env))
+		return expression.Evaluate(env), nil
 	}
 }
